e98d5eef887817b96e871586557ce71069f78d16
Buck <buck@buckrozelle.com>
2020-09-23 19:07:31 -0400

    Create the data model - Chapter 10.

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,18 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Buck Rozelle on 9/23/20.
+//  Copyright © 2020 buckrozelledotcomLLC. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+    
+    func toggleChecked() {
+        checked.toggle()
+    }
+}
