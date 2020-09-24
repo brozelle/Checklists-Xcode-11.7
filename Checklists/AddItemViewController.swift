8fedf2c389c1b8aa7d80bf4e2a06ece5516e9332
Buck <buck@buckrozelle.com>
2020-09-23 21:49:30 -0400

    Add the "Add Item" screen - Chapter 12.

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,14 +17,48 @@ class AddItemViewController: UITableViewController {
         
     }
     
-    // MARK:- Action Items?
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    
+    // MARK:- Actions
     
     @IBAction func cancel() {
         navigationController?.popViewController(animated: true)
     }
     
     @IBAction func done() {
+        
+        print("Contents of the text field: \(textField.text!)")
         navigationController?.popViewController(animated: true)
     }
+    
+    // MARK:- Table View Delegates
+    override func tableView(_ tableView: UITableView,
+                            willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
 
-}
+    // MARK:- Text Field Delegates
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in:oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        
+        doneBarButton.isEnabled = !newText.isEmpty
+        
+        /*if newText.isEmpty {
+            doneBarButton.isEnabled = false
+            } else {
+                doneBarButton.isEnabled = true
+            }*/
+            return true
+        }
+    }
+
